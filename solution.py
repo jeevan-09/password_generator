1d2bafcd29dae90ce13827465323fa296df50b3e
jeevan cu <jeevangowdacu@gmail.com>
2025-02-23 19:59:33 +0530

    Update solution.py

--- solution.py
+++ solution.py
@@ -8,20 +8,6 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-# Easy Level
-# password = ""
-# for char in range(0, nr_letters):
-#     password += random.choice(letters)
-#
-# for char in range(0, nr_symbols):
-#     password += random.choice(symbols)
-#
-# for char in range(0, nr_numbers):
-#     password += random.choice(numbers)
-#
-# print(password)
-
-# Hard level
 password_list = []
 for char in range(0, nr_letters):
     password_list.append(random.choice(letters))
